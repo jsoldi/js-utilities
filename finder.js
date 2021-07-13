7c4412163d2d6043fa03f607ce51e314e211a73e
jsoldi <jsoldi@hotmail.com>
2021-07-14 00:32:47 +0200

    Create finder.js

---
+++ finder.js
@@ -0,0 +1,19 @@
+function* findAnywhere(obj, text, maxDepth = 5, path = 'root') {
+	if (maxDepth > 0 && obj) {
+		switch (typeof obj) {
+			case 'string':
+			case 'number':
+				if (obj.toString().indexOf(text) !== -1) {
+					yield path;
+				}
+				break;
+			case 'object':
+				for (var entry of Object.entries(obj)) {
+					if (entry[1]) {
+						yield* findAnywhere(entry[1], text, maxDepth - 1, `${path}['${entry[0]}']`);
+					}
+				}
+				break;
+		}
+	}
+}
