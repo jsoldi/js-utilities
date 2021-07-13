b9603860ceb7ffb07ffca056f32a19a89aacbb01
jsoldi <jsoldi@hotmail.com>
2021-07-14 00:34:32 +0200

    Update finder.js

--- finder.js
+++ finder.js
@@ -9,9 +9,8 @@ function* findAnywhere(obj, text, maxDepth = 5, path = 'root') {
 				break;
 			case 'object':
 				for (var entry of Object.entries(obj)) {
-					if (entry[1]) {
+					if (entry[1]) 
 						yield* findAnywhere(entry[1], text, maxDepth - 1, `${path}['${entry[0]}']`);
-					}
 				}
 				break;
 		}
